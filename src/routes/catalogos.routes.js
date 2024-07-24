f8a87df3796ad3dc1eb1e6f021ebf29d8aeb71f5
infotareassoluciones <infotareassoluciones@gmail.com>
2024-07-23 20:50:07 -0500

    imagen

--- src/routes/catalogos.routes.js
+++ src/routes/catalogos.routes.js
@@ -8,6 +8,7 @@ router.get('/catalogos/list', async (req, res) => {
     try {
         const [catalogos] = await pool.query(
             'SELECT p.Descripcion, p.Precio, p.Imagen, p.Stock, p.Talla, c.Nombre AS Categoria, pr.Nombre AS PrendaNombre FROM Productos p INNER JOIN Categorias c ON p.CategoriaID = c.CategoriaID INNER JOIN Prendas pr ON p.PrendaID = pr.PrendaID ORDER BY pr.Nombre ASC;'
+            
         );
 
         // Convierte los datos de la imagen a base64
